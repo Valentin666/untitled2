b837eded1cd1ce736953c7a75bbfd09bf63f242f
Valentin666 <lita-stroy@mail.ru>
2018-10-05 04:15:25 +0500

    4 java config

--- src/main/java/org/doit/Main.java
+++ src/main/java/org/doit/Main.java
@@ -1,13 +1,19 @@
 package org.doit;
 
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+//import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
     public static void main(String[] args) {
-        ApplicationContext context = new ClassPathXmlApplicationContext("context.xml");
-        MessageRenderer renderer = context.getBean("renderer", MessageRenderer.class);
-        renderer.render();
+
+          ApplicationContext context = new AnnotationConfigApplicationContext(JavaConfig.class);
+          MessageRenderer renderer = context.getBean("renderer", MessageRenderer.class);
+          renderer.render();
+
+//        ApplicationContext context = new ClassPathXmlApplicationContext("context.xml");
+//        MessageRenderer renderer = context.getBean("renderer", MessageRenderer.class);
+//        renderer.render();
 
 //        MessageRenderer renderer = MessageFactory.getRenderer();
 //        renderer.render();
